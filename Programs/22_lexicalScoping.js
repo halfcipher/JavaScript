91c47264af0c81e7265b6b1614c7b2adbd32d175
Shubham Kadam <shubs3497@gmail.com>
2021-01-14 00:55:05 +0530

    Add files via upload

---
+++ Programs/22_lexicalScoping.js
@@ -0,0 +1,15 @@
+
+
+
+
+function init(){
+    var firstName = "shubham"
+    function sayFirstName(){
+        console.log(firstName);
+    }
+    sayFirstName();
+    //console.log(firstName);
+}
+
+init();
+console.log(firstName);
